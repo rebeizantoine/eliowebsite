9729a84f22d87a54b1015b90209363b11ea1b5e7
rebeizantoine <rebeizantoine@gmail.com>
2024-09-13 22:55:09 +0300

    changes done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -145,6 +145,7 @@ const Header = () => {
                 height="26"
                 viewBox="0 96 960 960"
                 width="26"
+                fill="#fff"
               >
                 <path d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" />
               </svg>
